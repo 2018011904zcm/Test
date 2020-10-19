80e1b65bed8aaeef6a401277b72e33be22c8fa29
zcm <2201668984@qq.com>
2020-10-19 14:42:09 +0800

    Signed-off-by: zcm <2201668984@qq.com>

---
+++ precache-manifest.38548907c65f4c20c95f0c2dd0d78818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60ec900f441fccb56e0679b0aafb56a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70071445305c138a98a2",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b3049a2a771c8bfe9ce4",
+    "url": "./static/css/main.8af6a133.chunk.css"
+  },
+  {
+    "revision": "70071445305c138a98a2",
+    "url": "./static/js/2.36ac912b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.36ac912b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3049a2a771c8bfe9ce4",
+    "url": "./static/js/main.2325dfae.chunk.js"
+  },
+  {
+    "revision": "aac7140d7b6a4fbae687",
+    "url": "./static/js/runtime-main.a0477606.js"
+  }
+]);
